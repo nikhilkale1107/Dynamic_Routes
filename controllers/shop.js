13d1885a86c869a5febbb97db03136153f004c8a
Nikhil Kale <nikhilkale1107@gmail.com>
2024-03-03 21:03:00 +0530

    Dynamic Routes I

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,8 +12,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req,res,next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
-  res.redirect('/');
+  Product.findById(prodId , product =>{
+    res.render('shop/product-detail', {product: product, pageTitle: product.Title, path: '/products' })
+  });
 }
 
 exports.getIndex = (req, res, next) => {
